25ce1b875d1d65d67e7e70d61513fd0d6e185099
Matt Eckhaus <matt.eckhaus@abc.net.au>
2017-09-05 13:49:52 +1000

    Works with photos and videos, multiple pluck instances

--- pluck.py
+++ pluck.py
@@ -8,6 +8,8 @@ import re
 import csv
 import sys
 
+debug = False
+
 cookies = {
     'anonId': '7c16e624-623f-4eab-b671-21c9d3f614f2',
     'SRV_ID': 'usw1web023.aws-us.pluck.com',
@@ -21,93 +23,95 @@ cookies = {
     'ASP.NET_SessionId': 'bmjojmybeykicjnrcsyftctg',
 }
 
-for media_type in ["video","photo"]:
+for domain in ["pluck2", "pluck"]:
+    print(domain)
+    
+    for media_type in ["video","photo"]:
+        print(media_type)
 
-    pluck_csv_file = open("pluck-%ss.csv" % media_type, 'w')
-    pluck_csv = csv.writer(pluck_csv_file, dialect='excel')
+        pluck_csv_file = open("%s-%ss.csv" % (domain, media_type), 'w')
+        pluck_csv = csv.writer(pluck_csv_file, dialect='excel')
 
-    gallery_regex = re.compile(
-        '<a href="http://pluck.abc.net.au/ver1.0/CMW/%ss/ManageApprovedSubmissions\?galleryKey=([^"]+)">([^<]+)</a>' % media_type, flags=re.M|re.I)
-    item_regex = re.compile(
-        '<a href="http://pluck.abc.net.au/ver1.0/CMW/%ss/%sDetail\?%sKey=([a-f0-9-]+)&galleryKey=([a-f0-9-]+)&?.*">([^<]+)</a>' % (media_type, media_type, media_type), flags=re.M|re.I)
-    item_description_regex = re.compile('<textarea\s+id="description".*?>(.*?)</textarea>', flags=re.M|re.S)
-    item_owner_regex = re.compile('http://pluck.abc.net.au/ver1.0/CMW/Users/User\?userKey=(?:expired_)?\d+">(.*?)</a>')
-    item_anon_owner_regex = re.compile('<span>\s*anonymous\s*(Anonymous)\s*</span>')
-    item_tags_regex = re.compile('<input type="text" id="tags" name="tags" value="(.*?)"\s*/>', flags=re.M|re.S)
-    next_gallery_page_regex = re.compile('<a href="(http://pluck.abc.net.au/ver1.0/CMW/%ss/\?itemsPerPage=\d+&nextItemOffset=d+&PagnAction=Next&searchString=)">Next</a>' % media_type, flags=re.I)
-    next_item_page_regex = re.compile('<a href="(http://pluck.abc.net.au/ver1.0/CMW/%ss/ManageApprovedSubmissions\?galleryKey=[a-f0-9-]+&itemsPerPage=\d+&nextItemOffset=\d+&PagnAction=Next)">Next</a>' % media_type, flags=re.I)
-    download_url_regex = re.compile('<a href="(http://pluck.abc.net.au/ver1.0/Content/%ss/Store/Main/[a-f0-9-]+/[a-f0-9-]+/[a-f0-9-]+\.[^"]+)"' % media_type, flags=re.I)
+        gallery_regex = re.compile(
+            '<a href="http://%s.abc.net.au/ver1.0/CMW/%ss/ManageApprovedSubmissions\?galleryKey=([^"]+)">([^<]+)</a>' % (domain,media_type), flags=re.M|re.I)
+        item_regex = re.compile(
+            '<a href="http://%s.abc.net.au/ver1.0/CMW/%ss/%sDetail\?[^"]*%sKey=([a-f0-9-]+)[^"]*">([^<]+)</a>' % (domain, media_type, media_type, media_type), flags=re.M|re.I)
+        item_description_regex = re.compile('<textarea\s+id="description".*?>(.*?)</textarea>', flags=re.M|re.S)
+        item_owner_regex = re.compile('http://%s.abc.net.au/ver1.0/CMW/Users/User\?userKey=(?:expired_)?\d+">(.*?)</a>' % (domain))
+        item_anon_owner_regex = re.compile('<span>\s*anonymous\s*(Anonymous)\s*</span>')
+        item_tags_regex = re.compile('<input type="text" id="tags" name="tags" value="(.*?)"\s*/>', flags=re.M|re.S)
+        next_gallery_page_regex = re.compile('<a href="(http://%s.abc.net.au/ver1.0/CMW/%ss/\?itemsPerPage=\d+&nextItemOffset=d+&PagnAction=Next&searchString=)">Next</a>' % (domain,media_type), flags=re.I)
+        next_item_page_regex = re.compile('<a href="(http://%s.abc.net.au/ver1.0/CMW/%ss/ManageApprovedSubmissions\?galleryKey=[a-f0-9-]+&itemsPerPage=\d+&nextItemOffset=\d+&PagnAction=Next)">Next</a>' % (domain,media_type), flags=re.I)
+        download_url_regex = re.compile('<a href="(http://%s.abc.net.au/ver1.0/Content/%ss/Store/Main/[a-f0-9-]+/[a-f0-9-]+/[a-f0-9-]+\.[^"]+)"' % (domain,media_type), flags=re.I)
+        
+        gallery_list_url ='http://%s.abc.net.au/ver1.0/CMW/%ss/?itemsPerPage=100&nextItemOffset=0&PagnAction=Next&searchString=' % (domain, media_type)
 
+        while gallery_list_url:
+            if debug:
+                print(gallery_list_url)
+            r1 = requests.get(gallery_list_url, cookies=cookies)
+            galleries = gallery_regex.findall(r1.text)
+            for g in galleries:
+                gallery_id = g[0]
+                if gallery_id in ('8d6e331b-0d00-4ed3-94f2-58e26b196a62','b4c68ba3-27d0-4355-a657-ba2d16cc5dd0'):
+                    continue
+                gallery_name = g[1]
+                if re.match('\d+ Photos', gallery_name):
+                    continue
+                gallery_url = "http://%s.abc.net.au/ver1.0/CMW/%ss/ManageApprovedSubmissions?galleryKey=%s&itemsPerPage=100&nextItemOffset=0&PagnAction=Next" % (domain, media_type, gallery_id)
+                if debug:
+                    print("Gallery URL: ", gallery_url)
+                while gallery_url:
+                    r2 = requests.get(gallery_url, cookies=cookies)
+                    items = item_regex.findall(r2.text)
+                    for i in items:
+                        item_id = i[0]
+                        item_title = i[1]
+                        item_url = "http://%s.abc.net.au/ver1.0/CMW/%ss/%sDetail?%sKey=%s&galleryKey=%s" % (domain, media_type, media_type, media_type, item_id, gallery_id)
+                        if debug:
+                            print(item_url)
+                        r3 = requests.get(item_url, cookies=cookies)
+                        description = None
+                        owner = None
+                        tags = None
+                        download_url = None
 
-
-    gallery_list_url ='http://pluck.abc.net.au/ver1.0/CMW/%ss/?itemsPerPage=100&nextItemOffset=0&PagnAction=Next&searchString=' % media_type
-
-
-    while gallery_list_url:
-        # print(gallery_list_url)
-        r1 = requests.get(gallery_list_url, cookies=cookies)
-        galleries = gallery_regex.findall(r1.text)
-        for g in galleries:
-            gallery_id = g[0]
-            if gallery_id in ('8d6e331b-0d00-4ed3-94f2-58e26b196a62','b4c68ba3-27d0-4355-a657-ba2d16cc5dd0'):
-                continue
-            gallery_name = g[1]
-            if re.match('\d+ Photos', gallery_name):
-                continue
-            gallery_url = "http://pluck.abc.net.au/ver1.0/CMW/%ss/ManageApprovedSubmissions?galleryKey=%s&itemsPerPage=100&nextItemOffset=0&PagnAction=Next" % (media_type, gallery_id)
-            # print(gallery_url)
-            while gallery_url:
-                r2 = requests.get(gallery_url, cookies=cookies)
-                items = item_regex.findall(r2.text)
-                for i in items:
-                    item_id = i[0]
-                    item_title = i[2]
-                    item_url = "http://pluck.abc.net.au/ver1.0/CMW/%ss/%sDetail?%sKey=%s&galleryKey=%s" % (media_type, media_type, media_type, item_id, gallery_id)
-                    # print(item_url)
-                    r3 = requests.get(item_url, cookies=cookies)
-                    description = None
-                    owner = None
-                    tags = None
-                    download_url = None
-
-                    try:
-                        m3 = item_description_regex.search(r3.text)
-                        description = m3.group(1)
-                    except AttributeError:
-                        print("Couldn't get description for %s %s in %s" % (media_type, item_id, gallery_id), file=sys.stderr)
-
-                    try:
-                        m3 = item_owner_regex.search(r3.text)
-                        owner = m3.group(1)
-                    except AttributeError:
                         try:
-                            m3 = item_anon_owner_regex.search(r3.text)
-                            owner = "anonymous"
+                            m3 = item_description_regex.search(r3.text)
+                            description = m3.group(1)
                         except AttributeError:
-                            print("Couldn't get owner for %s %s in %s" % (media_type, item_id, gallery_id), file=sys.stderr)
+                            print("Couldn't get description for %s %s in %s" % (media_type, item_id, gallery_id), file=sys.stderr)
 
-                    try:
-                        m3 = item_tags_regex.search(r3.text)
-                        tags = m3.group(1)
-                    except AttributeError:
-                        print("Couldn't get tags for %s %s in %s" % (media_type, item_id, gallery_id), file=sys.stderr)
+                        try:
+                            m3 = item_owner_regex.search(r3.text)
+                            owner = m3.group(1)
+                        except AttributeError:
+                            try:
+                                m3 = item_anon_owner_regex.search(r3.text)
+                                owner = "anonymous"
+                            except AttributeError:
+                                print("Couldn't get owner for %s %s in %s" % (media_type, item_id, gallery_id), file=sys.stderr)
 
-                    try:
-                        m3 = download_url_regex.search(r3.text)
-                        download_url = m3.group(1)
-                    except AttributeError:
-                        print("Couldn't get download URL for %s %s in %s" % (media_type, item_id, gallery_id), file=sys.stderr)
+                        try:
+                            m3 = item_tags_regex.search(r3.text)
+                            tags = m3.group(1)
+                        except AttributeError:
+                            print("Couldn't get tags for %s %s in %s" % (media_type, item_id, gallery_id), file=sys.stderr)
 
-                    pluck_csv.writerow([gallery_id, gallery_name, item_id, download_url, item_title, owner, tags, description])
-                m2 = next_item_page_regex.search(r2.text)
-                if m2:
-                    gallery_url = m2.group(1)
+                        # try:
+                        #     m3 = download_url_regex.search(r3.text)
+                        #     download_url = m3.group(1)
+                        # except AttributeError:
+                        #     print("Couldn't get download URL for %s %s in %s" % (media_type, item_id, gallery_id), file=sys.stderr)
+
+                        pluck_csv.writerow([gallery_id, gallery_name, item_id, download_url, item_title, owner, tags, description])
+                    m2 = next_item_page_regex.search(r2.text)
+                    if m2:
+                        gallery_url = m2.group(1)
+                    else:
+                        gallery_url = None
+                m1 = next_gallery_page_regex.search(r1.text)
+                if m1:
+                    gallery_list_url = m1.group(1)
                 else:
-                    gallery_url = None
-            m1 = next_gallery_page_regex.search(r1.text)
-            if m1:
-                gallery_list_url = m1.group(1)
-            else:
-                gallery_list_url = None
-
-            
+                    gallery_list_url = None
