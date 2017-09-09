69d9c6cdac0f828f3d167f6da1e298f4e038abaa
Matt Eckhaus <matt@eckha.us>
2017-09-10 09:56:14 +1000

    Get media

--- get_media.py
+++ get_media.py
@@ -7,24 +7,49 @@ import csv
 import sys
 import requests
 import os
+import boto3
 
-DIR = sys.argv[1] + "-SAVED"
+debug = True
 
-csv_filename = sys.argv[1] + ".csv"
+domain = sys.argv[1]
+media_type = sys.argv[2]
 
-with open(csv_filename, newline='') as csvfile:
+assert(domain == "pluck" or domain == "pluck2")
+assert(media_type == "Photo" or media_type == "Video" or media_type == "Blog")
+
+
+csv_filename = "%s-%ss.csv" % (domain, media_type)
+logfilename = "%s-%ss-download.log" % (domain, media_type)
+
+logfile = open(logfilename, 'a')
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('pluck-export')
+objects = bucket.objects.filter(Prefix='%s/%s' % (domain, media_type))
+size = {}
+for o in objects:
+    size[o.key] = o.size
+
+with open(csv_filename, 'r') as csvfile:
     csvreader = csv.reader(csvfile, dialect='excel')
     for row in csvreader:
         url = row[3]
-        savefile = DIR + "/" + url.split('/')[-1]
-        if os.path.isfile(savefile):
+        if (url == ''):
+            continue
+        gallery_id = row[0]
+        item_id = row[2]
+        filename = url.split('/')[-1]
+        keyname = '%s/%s/%s/%s' % (domain, media_type, gallery_id, filename)
+        print(keyname)
+        if keyname in size:
             r = requests.head(url, allow_redirects=True)
             online_size = r.headers['content-length']
-            file_size = os.path.getsize(savefile)
-            if int(online_size) == file_size:
+            if int(online_size) == size[keyname]:
+                if debug:
+                    print("Skipping %s size %s" % (keyname, online_size))
                 continue
         r = requests.get(url, allow_redirects=True)
         if r.status_code == 200:
-            open(savefile, 'wb').write(r.content)
+            o = bucket.put_object(Key=keyname, Body=r.content)
         else:
-            print("Failed %s" % url)
+            print("%s %s" % (r.status_code, url), file=logfile)
